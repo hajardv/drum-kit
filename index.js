3259ee8d5bb77897bb07e2fd8fdc8912b90e1b2a
yassineboucham <yassineboucham11@email.com>
2024-12-03 16:48:12 +0100

    all false for the prject

---
+++ index.js
@@ -0,0 +1,14 @@
+for(i=0;i<=7;i++){
+        
+ let btn =   document.querySelectorAll("button")[i]
+
+ btn.addEventListener("click",click)
+}
+
+
+function click(){
+    alert("w")
+}
+
+    
+        
